dc90694b756ac4380421732c31b1beaa4b4aaf99
Dan Serban <dserban01@gmail.com>
2012-10-27 23:42:37 +0300

    RED: User: test_that_it_responds_to_ratings

--- testharness.rb
+++ testharness.rb
@@ -2,6 +2,9 @@ require 'test/unit'
 require './MovieRecommender.rb'
 
 class TestUserModel < Test::Unit::TestCase
+  def test_that_it_responds_to_ratings
+    assert_respond_to( User.new, :ratings, failure_message = "It does not traverse the foreign key relationship." )
+  end
 end
 
 class TestMovieModel < Test::Unit::TestCase
