729d8e26964db066b8931482d15b4b1e112048e1
Dan Serban <dserban01@gmail.com>
2012-10-28 02:11:32 +0300

    RED: User: test_that_it_checks_for_presence_of_nickname

--- testharness.rb
+++ testharness.rb
@@ -8,6 +8,11 @@ class TestUserModel < Test::Unit::TestCase
   def test_that_it_responds_to_movies
     assert_respond_to( User.new, :movies, failure_message = "It does not traverse the foreign key relationship." )
   end
+  def test_that_it_checks_for_presence_of_nickname
+    u = User.new
+    u.valid?
+    assert( u.errors.messages.has_key?(:nickname), failure_message = "It does not check for the presence of attribute nickname." )
+  end
 end
 
 class TestMovieModel < Test::Unit::TestCase
