846d32c2be38e310fca1ba1f4fb10e852c9150de
Dan Serban <dserban01@gmail.com>
2012-10-28 02:03:31 +0300

    Added behavior to switch between users

--- MovieRecommender.rb
+++ MovieRecommender.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 
+enable :sessions
+
 configure do
   options = { :adapter => "sqlite3", :database => "dev.db" }
   ActiveRecord::Base.establish_connection(options)
@@ -21,8 +23,24 @@ class Rating < ActiveRecord::Base
   belongs_to :movie
 end
 
+before do
+  session[:signed_in_user_id] ||= 1
+  @signed_in_user_name = User.find_by_id(session[:signed_in_user_id]).nickname
+  @my_user_id = session[:signed_in_user_id].to_i
+end
+
 get "/" do
   @movies = Movie.all
   erb :welcomepage
+end
+
+get "/signinform" do
+  @users = User.all
+  erb :signinform
+end
+
+post "/dosignin/:id" do
+  session[:signed_in_user_id] = params[:id].to_i
+  redirect "/"
 end
 
