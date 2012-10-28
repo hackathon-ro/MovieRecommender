5e5d66dc1b988c10b5f1f1304ebc6715c47294b9
Dan Serban <dserban01@gmail.com>
2012-10-28 04:04:29 +0200

    Seeded the database with a few ratings

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,3 +39,28 @@ users.each do |u|
   User.create(u)
 end
 
+u = User.find_by_nickname("Frodo")
+r = Rating.create(:user => u, :movie => Movie.find_by_name("The Matrix"), :rating => 4)
+r = Rating.create(:user => u, :movie => Movie.find_by_name("Inception"), :rating => 4)
+r = Rating.create(:user => u, :movie => Movie.find_by_name("Prometheus"), :rating => 5)
+r = Rating.create(:user => u, :movie => Movie.find_by_name("TRON: Legacy"), :rating => 5)
+r = Rating.create(:user => u, :movie => Movie.find_by_name("Titanic"), :rating => 2)
+r = Rating.create(:user => u, :movie => Movie.find_by_name("Casablanca"), :rating => 1)
+r = Rating.create(:user => u, :movie => Movie.find_by_name("Shakespeare in Love"), :rating => 1)
+r = Rating.create(:user => u, :movie => Movie.find_by_name("Total Recall"), :rating => 5)
+
+u = User.find_by_nickname("AgentSmith")
+r = Rating.create(:user => u, :movie => Movie.find_by_name("The Matrix"), :rating => 5)
+r = Rating.create(:user => u, :movie => Movie.find_by_name("Inception"), :rating => 5)
+r = Rating.create(:user => u, :movie => Movie.find_by_name("Prometheus"), :rating => 4)
+r = Rating.create(:user => u, :movie => Movie.find_by_name("TRON: Legacy"), :rating => 4)
+r = Rating.create(:user => u, :movie => Movie.find_by_name("The Avengers"), :rating => 5)
+r = Rating.create(:user => u, :movie => Movie.find_by_name("Iron Man"), :rating => 4)
+r = Rating.create(:user => u, :movie => Movie.find_by_name("Avatar"), :rating => 5)
+r = Rating.create(:user => u, :movie => Movie.find_by_name("Equilibrium"), :rating => 4)
+r = Rating.create(:user => u, :movie => Movie.find_by_name("Pirates of Silicon Valley"), :rating => 5)
+r = Rating.create(:user => u, :movie => Movie.find_by_name("Midnight in Paris"), :rating => 1)
+r = Rating.create(:user => u, :movie => Movie.find_by_name("Titanic"), :rating => 1)
+r = Rating.create(:user => u, :movie => Movie.find_by_name("Casablanca"), :rating => 2)
+r = Rating.create(:user => u, :movie => Movie.find_by_name("L.A. Confidential"), :rating => 1)
+
