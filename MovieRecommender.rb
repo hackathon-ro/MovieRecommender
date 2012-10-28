9c5ed777bb19ac069ec388463ec61734a6af5602
Dan Serban <dserban01@gmail.com>
2012-10-28 05:07:59 +0200

    GREEN: Rating: test_that_it_checks_that_the_rating_is_lteq_5

--- MovieRecommender.rb
+++ MovieRecommender.rb
@@ -23,7 +23,8 @@ end
 class Rating < ActiveRecord::Base
   validates :user_id, :movie_id, :rating, :presence => true
   validates :rating, :numericality => { :only_integer => true,
-                                        :greater_than_or_equal_to => 1 }
+                                        :greater_than_or_equal_to => 1,
+                                        :less_than_or_equal_to => 5 }
   belongs_to :user
   belongs_to :movie
 end
