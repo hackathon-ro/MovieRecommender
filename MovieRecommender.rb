4d497277e94ebbcc40e3bfd5d29fae09403f2078
Dan Serban <dserban01@gmail.com>
2012-10-28 04:27:58 +0200

    GREEN: Rating: test_that_it_checks_that_the_rating_is_gteq_1

--- MovieRecommender.rb
+++ MovieRecommender.rb
@@ -22,7 +22,8 @@ end
 
 class Rating < ActiveRecord::Base
   validates :user_id, :movie_id, :rating, :presence => true
-  validates :rating, :numericality => { :only_integer => true }
+  validates :rating, :numericality => { :only_integer => true,
+                                        :greater_than_or_equal_to => 1 }
   belongs_to :user
   belongs_to :movie
 end
